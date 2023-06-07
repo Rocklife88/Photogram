e5e5354b42b8092d352b49061046926f4858bb61
rocklife88 <pelleu2@gmail.com>
2023-06-07 17:13:17 +0200

    Navbar added

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Nav from "./components/Nav.vue"
+
+</script>
+
+
+
+<template>
+<main>
+  <Nav/>
+</main>
+
+
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,81 @@
+<script setup>
+import {RouterLink} from "vue-router"
+import {ref} from "vue"
+import Container from "./Container.vue"
+
+
+const searchUsername= ref("")
+const onSearch = () => {}
+
+
+</script>
+
+
+
+<template>
+
+        <ALayoutHeader>
+ <Container>
+        <div class="nav-container">
+                <div class="right-content">
+                        <RouterLink to="/">PhotoGram</RouterLink>
+                        <AInputSearch
+                        v-model:value="searchUsername"
+                        placeholder="Username..."
+                        style="width: 200px"
+                        @search="onSearch"
+                      />
+                     
+
+                </div>
+
+                <div class="left-content">
+                        <AButton type="primary">Sign Up</AButton>
+                        <AButton type="primary">Login</AButton>
+
+                </div>
+            
+
+
+        </div>
+
+     
+ </Container>
+                  
+
+     
+    
+
+
+          </ALayoutHeader>
+         
+
+
+</template>
+
+
+
+<style scoped>
+
+.nav-container{
+        display:flex;
+        align-items:center;
+        justify-content: space-between;
+}
+
+.right-content{
+        display:flex;
+        align-items: center;
+
+}
+
+.right-content a{
+        margin-right:10px;
+}
+
+.left-content button{
+        margin-left:10px;
+}
+
+
+</style>
