540e9be50f4f3ffcc6e27b2866f9fe40de495a4d
Rocklife88 <118614412+Rocklife88@users.noreply.github.com>
2023-06-08 15:16:38 +0200

    user profile added

---
+++ src/components/Profile.vue
@@ -0,0 +1,45 @@
+
+
+<script setup>
+import Container from './Container.vue';
+import UserBar from "./UserBar.vue"
+import ImageGallery from './ImageGallery.vue';
+
+
+
+
+</script>
+
+
+<template>
+<Container>
+    <div class="profile-container">
+ <UserBar
+ username="marcopelelgrini"
+ :userInfo="{
+    posts:4,
+    followers:100,
+    following:342
+
+ }"
+ />
+<ImageGallery :posts="[{
+    id:1,
+    image:'https://xsgames.co/randomusers/assets/avatars/male/63.jpg'
+
+}]"/>
+</div>
+</Container>
+</template>
+
+
+
+<style scoped>
+
+.profile-container{
+    display:flex;
+    flex-direction:column;
+    padding:20px 0;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfileView from '../views/ProfileView.vue'
 
 
 
@@ -10,6 +11,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/profile/:username',
+      name: 'profile',
+      component: ProfileView
     },
    
    
