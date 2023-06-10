40ade4f5f35e97adcc36e24ce99706ffca54de82
Marco Pellegrini <118614412+Rocklife88@users.noreply.github.com>
2023-06-10 10:24:51 +0000

    added error message and auth

--- src/stores/users.js
+++ src/stores/users.js
@@ -3,12 +3,35 @@ import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('users', () => {
   const user = ref(null);
+  const errorMessage = ref("")
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
   const handleLogin = () => {}
 
 
 
-  const handleSignUp = () => {}
+  const handleSignUp = (credentials) => {
+    const {email, password,username} = credentials
+
+    if ( password.length < 6){
+      return errorMessage.value ="password needs to be at least 6 characters"
+    }
+
+  if (!validateEmail(email)){
+    return errorMessage.value ="email is not valid"
+  }
+
+  if ( username.length < 4){
+    return errorMessage.value ="username needs to be at least 6 characters"
+  }
+}
 
 
 
@@ -20,5 +43,6 @@ export const useUserStore = defineStore('users', () => {
 
 
 
-  return { user, handleLogout, handleSignUp,handleLogin, getUser }
+  return { user, errorMessage, handleLogout, handleSignUp,handleLogin, getUser }
 })
+
