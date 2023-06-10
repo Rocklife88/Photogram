b9b107dc76404dba3ff7f7065adf839f57bf2b28
Marco Pellegrini <118614412+Rocklife88@users.noreply.github.com>
2023-06-10 11:34:00 +0000

    connected frontend to supabase backend auth

--- src/stores/users.js
+++ src/stores/users.js
@@ -1,5 +1,6 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { supabase } from '../supabase';
 
 export const useUserStore = defineStore('users', () => {
   const user = ref(null);
@@ -17,20 +18,39 @@ export const useUserStore = defineStore('users', () => {
 
 
 
-  const handleSignUp = (credentials) => {
+  const handleSignUp = async (credentials) => {
     const {email, password,username} = credentials
 
     if ( password.length < 6){
       return errorMessage.value ="password needs to be at least 6 characters"
     }
 
-  if (!validateEmail(email)){
-    return errorMessage.value ="email is not valid"
+ if (!validateEmail(email)){
+   return errorMessage.value ="email is not valid"
   }
 
   if ( username.length < 4){
     return errorMessage.value ="username needs to be at least 6 characters"
   }
+  errorMessage.value=""
+
+  // validate if users exists
+
+  const {error} =  await supabase.auth.signUp({
+    email,
+    password
+  })
+
+  if(error){
+    return errorMessage.value = error.message
+  }
+
+await supabase.from("users").insert({
+  username,
+  email
+
+})
+
 }
 
 
@@ -45,4 +65,3 @@ export const useUserStore = defineStore('users', () => {
 
   return { user, errorMessage, handleLogout, handleSignUp,handleLogin, getUser }
 })
-
